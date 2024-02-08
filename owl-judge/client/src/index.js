a542fbcc21a82af9e652aa02555ff39a7f18afbc
Haktan Guzel <haktanguzelswe@gmail.com>
2024-02-07 22:25:20 -0500

    Firebase Hosting and Deployment
    Firebase Project was Deployed

--- owl-judge/client/src/index.js
+++ owl-judge/client/src/index.js
@@ -3,6 +3,27 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAaHXfEvKTF01TuJIf50ZVJe_nlnuU4QPU",
+  authDomain: "owl-judge.firebaseapp.com",
+  projectId: "owl-judge",
+  storageBucket: "owl-judge.appspot.com",
+  messagingSenderId: "339201974480",
+  appId: "1:339201974480:web:20acafd7aea7b25fe63ed5",
+  measurementId: "G-45D4H0G9WJ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
